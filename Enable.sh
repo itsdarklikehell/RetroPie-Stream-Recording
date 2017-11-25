ffaa6ec01c7a8adfcf28a1a98e9f6f7efadaa6e9
itsdarklikehell <bauke.molenaar@gmail.com>
2017-11-25 23:11:57 +0100

    modified:   Enable.sh

--- Enable.sh
+++ Enable.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 echo "Enableing Stream Recording."
+nano /home/pi/RetroPie/recording/twitch.cfg
+
+echo "vcodec = libx264" > /home/pi/RetroPie/recording/twitch.cfg
+acodec = libfdk_aac
+pix_fmt = yuv420p
+threads = 2
+scale_factor = 1
+format = flv
+video_preset = ultrafast
+video_profile = main
+video_tune = animation
+video_bufsize = 512k
+video_minrate = 512k
+video_maxrate = 512k
+video_r = 60
+video_g = 120
+video_keyint_min = 60
+sample_rate = 44100
+audio_preset = aac_he_v2
+audio_global_quality = 1
